6fe9f6636dbf23f9b89a3c0e64cdb36566104b27
luisaguiar <luisaguiar@duck.com>
2025-04-19 13:21:57 +0100

    tests

---
+++ ft_printf.c
@@ -0,0 +1,51 @@
+#include "libft/libft.h"
+#include <stdarg.h>
+
+int	ft_printf(const char *str, ...)
+{
+	int		count;
+	va_list	args;
+	int		i;
+
+	va_start(args, str);
+	while (str[i])
+	{
+		if (*str != '%')
+		{
+			i++;
+			write(1, &str[i], 1);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (str[i] == 'c')
+			{
+				ft_putchar_fd(va_arg(args, char), 1);
+				i++;
+				count++;
+			}
+			else if (str[i] == 'd')
+			{
+				ft_putnbr_fd(va_arg(args, int), 1);
+				i++;
+				count++;
+			}
+		}
+	}
+	va_end(args);
+	return (count);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	c = 'a';
+	int		d = 1;
+
+	int		count = ft_printf("Print an a: %c\nPrint a 1: %d\n", c, d);
+	int		count2 = printf("Print an a: %c\nPrint a 1: %d\n", c, d);
+	
+	printf("Count 1: %d\nCount 2: %d\n", count, count2);
+}
