17e1b2fce2abda63a40a2da454da418be3ddb01d
luisaguiar <luisaguiar@duck.com>
2025-04-20 21:06:49 +0100

    its smt..

---
+++ ft_printf.h
@@ -0,0 +1,10 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+typedef struct	s_fncts
+{
+	char	fs;
+	void	*fnct;
+}				t_fncts;
+
+#endif
--- try2_printf.c
+++ try2_printf.c
@@ -39,7 +39,35 @@ int	ft_printf(const char *str, ...)
 	return (count);
 }
 
+void	*get_function(char c)
+{
+	t_functs	fnct_arr[];
+	int			i;
 
+	i = -1;
+	fnct_arr[0].fs = 'c';
+	fnct_arr[0].fnct = ft_putchar;
+	fnct_arr[1].fs = 's';
+	fnct_arr[1].fnct = ft_putstr;
+	fnct_arr[2].fs = 'i';
+	fnct_arr[2].fnct = ft_putnbr;
+	fnct_arr[3].fs = 'd';
+	fnct_arr[3].fnct = ft_putnbr;
+	fnct_arr[4].fs = 'u';
+	fnct_arr[4].fnct = ft_putnbr_u;
+	fnct_arr[5].fs = 'x';
+	fnct_arr[5].fnct = ft_putnbr_hex;
+	fnct_arr[6].fs = 'X';
+	fnct_arr[6].fnct = ft_putnbr_hex;
+	fnct_arr[7].fs = 'p';
+	fnct_arr[7].fnct = ft_putnbr_hex;
+	fnct_arr[8].fs = '%';
+	fnct_arr[8].fnct = ft_print_percent;
+	while (++i < 9)
+		if (c == fnct_arr[i].fs)
+			return (fnct_arr[i].fnct);
+	return (0);
+}
 
 #include <stdio.h>
 
