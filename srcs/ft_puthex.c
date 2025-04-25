6a18c3c581180cea2ef2d3df52ed4fc88fb047e7
luisaguiar <luisaguiar@duck.com>
2025-04-25 11:07:00 +0100

    puthex upd

--- srcs/ft_puthex.c
+++ srcs/ft_puthex.c
@@ -10,16 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <unistd.h>
 
-void	ft_putnbr_hex(unsigned int n)
+void	ft_puthex(unsigned int n)
 {
 	unsigned char	c;
-	const char		*hex[16];
+	const char		*hex;
 
 	hex = "0123456789abcdef";
-	if (n > 9)
-		ft_putnbr(n / 10);
-	c = (n % 10) + '0';
+	if (n > 15)
+		ft_puthex(n / 16);
+	c = hex[n % 16];
 	write(1, &c, 1);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	ft_puthex(0);
+	printf("|%x\n", 0);
+	ft_puthex(5);
+	printf("|%x\n", 5);
+	ft_puthex(42);
+	printf("|%x\n", 42);
+	ft_puthex(101230);
+	printf("|%x\n", 101230);
+	ft_puthex(-123094);
+	printf("|%x\n", -123094);
+	ft_puthex(-2147483647);
+	printf("|%x\n", -2147483647);
+	ft_puthex(2147483647);
+	printf("|%x\n", 2147483647);
 }
