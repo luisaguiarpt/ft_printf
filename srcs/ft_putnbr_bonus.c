104df8735db297fbd9aa0225567d537167445121
luisaguiar <luisaguiar@duck.com>
2025-05-01 20:59:34 +0100

    seems like putnbr_format is working, still need to test more

--- srcs/ft_putnbr_bonus.c
+++ srcs/ft_putnbr_bonus.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_bonus.c                                  :+:      :+:    :+:   */
+/*   put_nbr_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldias-da <ldias-da@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -13,15 +13,15 @@
 #include "../includes/ft_printf_bonus.h"
 #include "../libft/libft.h"
 
-size_t	put_spaces(int n, t_format *format);
+static size_t	put_spaces(int n, t_format *format);
+static size_t	put_zeroes(int n, t_format *format);
+static size_t	put_nbr(int n);
 
 size_t	putnbr_format(int n, t_format *format)
 {
-	size_t	zeroes;
 	size_t	count;
 
 	count = 0;
-	zeroes = format->max - nbr_dig(n) + (n < 0);
 	if (!format->minus)
 		count += put_spaces(n, format);
 	if (n < 0)
@@ -29,18 +29,38 @@ size_t	putnbr_format(int n, t_format *format)
 		write(1, "-", 1);
 		count++;
 	}
-	while (zeroes--)
+	if (format->plus && n > 0)
 	{
-		write(1, "0", 1);
+		write(1, "+", 1);
 		count++;
 	}
-	count += ft_putnbr(n * ((n < 0) * -1 + 1 * (n > 0)));
+	count += put_zeroes(n, format);
+	count += put_nbr(n * ((n < 0) * -1 + 1 * (n > 0)));
 	if (format->minus)
 		count += put_spaces(n, format);
 	return (count);
 }
 
-size_t	put_spaces(int n, t_format *format)
+static size_t	put_zeroes(int n, t_format *format)
+{
+	size_t	zeroes;
+	size_t	i;
+
+	if (!format->zero)
+		zeroes = format->max - nbr_dig(n) + (n < 0);
+	else
+		zeroes = format->min - nbr_dig(n);
+	if (format->plus && n > 0)
+		zeroes -= 1;
+	while (zeroes--)
+	{
+		write(1, "0", 1);
+		i++;
+	}
+	return (i);
+}
+
+static size_t	put_spaces(int n, t_format *format)
 {
 	size_t	min;
 	size_t	max;
@@ -49,6 +69,8 @@ size_t	put_spaces(int n, t_format *format)
 	min = format->min;
 	max = format->max;
 	i = 0;
+	if (format->zero)
+		return (0);
 	if (min > nbr_dig(n) || min > max)
 		while (i < min - max - (n < 0))
 		{
@@ -58,7 +80,7 @@ size_t	put_spaces(int n, t_format *format)
 	return (i);
 }
 
-size_t	ft_putnbr(int n)
+static size_t	put_nbr(int n)
 {
 	char	c;
 	size_t	count;
@@ -76,7 +98,7 @@ size_t	ft_putnbr(int n)
 		write(1, "-", 1);
 	}
 	if (n > 9)
-		count += ft_putnbr(n / 10);
+		count += put_nbr(n / 10);
 	c = (n % 10) + '0';
 	write(1, &c, 1);
 	count++;
@@ -96,35 +118,37 @@ int	main(void)
 	format->min = 9;
 	format->max = 6;
 	format->minus = 0;
+	format->zero = 1;
+	format->plus = 1;
 
 	n = putnbr_format(1243, format);
 	printf("|%i\n", n);
-	m = printf("%9.6i", 1243);
+	m = printf("%+09i", 1243);
 	printf("|%i\n", m);
 	printf("------------------\n");
 	n = putnbr_format(1234, format);
 	printf("|%i\n", n);
-	m = printf("%9.6i", 1234);
+	m = printf("%+09i", 1234);
 	printf("|%i\n", m);
 	printf("------------------\n");
 	n = putnbr_format(9876, format);
 	printf("|%i\n", n);
-	m = printf("%9.6i", 9876);
+	m = printf("%+09i", 9876);
 	printf("|%i\n", m);
 	printf("------------------\n");
 	n = putnbr_format(-1243, format);
 	printf("|%i\n", n);
-	m = printf("%9.6i", -1243);
+	m = printf("%+09i", -1243);
 	printf("|%i\n", m);
 	printf("------------------\n");
 	n = putnbr_format(-21648, format);
 	printf("|%i\n", n);
-	m = printf("%9.6i", -21648);
+	m = printf("%+09i", -21648);
 	printf("|%i\n", m);
 	printf("------------------\n");
 	n = putnbr_format(2147, format);
 	printf("|%i\n", n);
-	m = printf("%9.6i", 2147);
+	m = printf("%+09i", 2147);
 	printf("|%i\n", m);
 }
 
